aa939af63d0f0f4f759eff6b5cc7dba82503892a
João Pedro <joaopedropereira3232@hotmail.com>
2024-01-25 10:32:12 -0300

    creating fetch all currencies method

---
+++ lib/infra/data/http/http_currency_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:realtime_quotation/infra/contracts/repositories/currency_repository.dart';
+import 'package:realtime_quotation/models/currency.dart';
+
+class HttpCurrencyRepository implements ICurrencyRepository {
+  final _client = http.Client();
+
+  @override
+  Future<List<Currency>> fetchAll() async {
+    try {
+      final url = Uri.parse('https://economia.awesomeapi.com.br/json/available/uniq');
+      final response = await _client.get(url);
+
+      if (response.statusCode != 200) {
+        throw Exception(response.reasonPhrase);
+      }
+
+      final body = response.body;
+      final map = jsonDecode(body) as Map<String, dynamic>;
+      final currencies = map.entries.map((entry) => Currency(code: entry.key, title: entry.value)).toList();
+      return currencies;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
